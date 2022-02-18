6ce162efcdadb3f2e9e8ecc5ae0119e57bcf1018
Dominik Przychodni <dominik4658@gmail.com>
2022-02-18 11:03:36 +0000

    Added commments

--- client.py
+++ client.py
@@ -4,37 +4,46 @@
 
 import socket
 import threading
+import select
 import time 
 import sys
-
-HEADER_LEN = 4     # This goes in front of the message and contains the length of the unpadded message
-MESSAGE_LEN = 2048  # Total length of the padded message
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DIS"
-CLIENT_IDS = ['A', 'B', 'C']
-HOST = '127.0.0.1' # Localhost
-PORT = 6032        
+from helperFunctions import *
 
 # Function to send a message to the server
-def send(msg):
-    # Encode the message
-    message = msg.encode(FORMAT)
-    
-    # Create a header containing message length
-    header = str(len(message))
-    header = header.encode(FORMAT)
-    if len(header) > HEADER_LEN:
-        print("Error: message too long")
-        return
-    # Pad the header
-    header += b' ' * (HEADER_LEN - len(header))
-
-    # Append the header in front of the message
-    message = header + message 
-
-    # Pad the message
-    message += b' ' * (MESSAGE_LEN - len(message))
+def send(msg, client):
+    message = encodeMessage(msg)
     client.send(message)
+
+def main(CLIENT):
+    # Create a client socket
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    # Connect to the server
+    client.connect((HOST, PORT))
+    print(f"Sending {CLIENT}")
+    send(CLIENT, client)
+    
+    # Receive and send messages
+    while True:
+        sockets_list = [sys.stdin, client]
+        read_sockets, write_socket, error_socket = select.select(sockets_list, [], [])
+        
+        for socks in read_sockets:
+            if socks == client:
+                message = socks.recv(MESSAGE_LEN)
+                _, message = decodeMessage(message)
+                print(f"{message[0]}: {message[1:]}") 
+            
+            else:
+                message = sys.stdin.readline()
+                sys.stdout.write(f"{CLIENT}:")
+                sys.stdout.write(message)
+                sys.stdout.flush()
+                send(message, client)
+
+                if message == DISCONNECT_MESSAGE:
+                    exit()
+
 
 if __name__ == "__main__":
     # Check if client ID was provided
@@ -47,16 +56,7 @@ if __name__ == "__main__":
     else:
         print(f"Error: Client ID not provided")
         print(f"Usage: $python3 client.py <ID>")
-
-    # Create a client socket
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
-    # Connect to the server
-    client.connect((HOST, PORT))
-    send(CLIENT)
+    main(CLIENT)
 
-    while True:
-        message = input("$")
-        send(message)
-        if message == DISCONNECT_MESSAGE:
-            exit()
+    
